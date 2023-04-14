68c21f2d82bcb499916b931b3322ead50b6467f8
Joseph Cho Nurukurthi <jcho@softwareag.com>
2023-04-14 23:07:53 +0530

    updated

--- store_results.sh
+++ store_results.sh
@@ -5,7 +5,7 @@ store_test_results()
         cat temp.txt
         run_id=$(cat temp.txt)
         echo run id: $run_id
-        rm -rf TEMP_DIRECTORY/$workload.csv
+        rm -rf TEMP_DIRECTORY/$testcase.csv
         sleep 1s
         cp -rf TEMP_DIRECTORY/$workload.csv TEMP_DIRECTORY/$testcase.csv
         java -DTEST_MASTER_KEY=ORIGIN_AWS_BPT -DRUN_ID=$run_id -DRETRY_ID=0 -DWORKLOAD_NAME=$testcase -DBASELINE_VERSION=10.15 -DPRODUCT_BUILD_NUMBER=ORIGIN_10.16.1.0 -DINSTALLED_INT_FIXES=None -DINSTALLED_GA_FIXES=None -DBUILD_NUMBER_OTHERS=None -DINSTALLED_FIXES_OTHERS=None -DTEST_DURATION=$duration -DCONCURRENT_USERS=$concurrentusers -DTEMP_DIRECTORY=TEMP_DIRECTORY/ -DSERVERS_TO_BE_MONITORED=ORIGIN-NA -cp bpt_utils.jar perf.bpt.ProcessTestResults
