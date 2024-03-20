3423f851e7b0b209e6bc19b229fe9098add8993c
Joseph Cho Nurukurthi <jcho@softwareag.com>
2024-03-20 16:35:43 +0530

    updated

--- edge_docker_image.sh
+++ edge_docker_image.sh
@@ -3,22 +3,21 @@ pull_edge_runtime_image(){
     docker pull iregistry.eur.ad.sag/aim/msr-edge-runtime:int
     IMAGE=iregistry.eur.ad.sag/aim/msr-edge-runtime:int
     date=`date +"%d-%m-%Y"`
-    docker save -o $1/msr-edge-runtime-$date.tar $IMAGE
-    echo "Saved $IMAGE to $1/msr-edge-runtime-$date.tar"
+    docker save -o $CURRENT_DIRECTORY/msr-edge-runtime-$date.tar $IMAGE
+    echo "Saved $IMAGE to $CURRENT_DIRECTORY/msr-edge-runtime-$date.tar"
 }
 
 load_edge_runtime_image(){
-    docker load -i $1/msr-edge-runtime-$date.tar
+    docker load -i $CURRENT_DIRECTORY/msr-edge-runtime-$date.tar
     docker image tag 5f3c58976aec iregistry.eur.ad.sag/aim/msr-edge-runtime:int
-
 }
 
 echo " Check the docker version "
 docker --version
+CURRENT_DIRECTORY=$PWD
 if [[ $1 == *"pull_edge_runtime_image"* ]]; then
-        pull_edge_runtime_image
+        pull_edge_runtime_image $CURRENT_DIRECTORY
 fi
 if [[ $1 == *"load_edge_runtime_image"* ]]; then
-        load_edge_runtime_image
+        load_edge_runtime_image $CURRENT_DIRECTORY
 fi
-
