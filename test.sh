c639d6986b2444e7a5ee86b391bc2ce2eb8bd3ed
Joseph Cho Nurukurthi <jcho@softwareag.com>
2024-03-22 03:01:10 +0530

    updated

--- test.sh
+++ test.sh
@@ -66,7 +66,7 @@ execute_jmeter_script()
         echo " execute jmeter script $testcase"
         $JMETER_HOME/bin/jmeter.sh -n -t JMeterScripts/$2.jmx -Jthreads=$3 -Jduration=$4 -l TEMP_DIRECTORY/$1.jtl
         java -jar $JMETER_HOME/lib/cmdrunner-2.2.jar --tool Reporter --generate-csv TEMP_DIRECTORY/$1.csv --input-jtl TEMP_DIRECTORY/$1.jtl --plugin-type SynthesisReport
-        cat $5/$testcase.csv
+        cat TEMP_DIRECTORY/$testcase.csv
         rm -f TEMP_DIRECTORY/$1.jtl
         java -DWORKLOAD_NAME=$1 -DTEMP_DIRECTORY=$5/ -cp bpt_utils.jar perf.bpt.util.CompareBaseline
 }
