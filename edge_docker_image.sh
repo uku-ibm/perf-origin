8a67da0d5a7d40b67f01e6921b682b8b6ebfa844
Joseph Cho Nurukurthi <jcho@softwareag.com>
2024-03-22 02:11:06 +0530

    updated

--- edge_docker_image.sh
+++ edge_docker_image.sh
@@ -17,7 +17,7 @@ load_edge_runtime_image(){
 
 delete_existing_edge_runtime_from_tenant(){
     echo "Search for perftest edge server from the tenant"
-    response=$(curl -s -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$TENANT_URL/integration/rest/edge/runtimes?page=1&limit=19&searchKey=$REMOTE_EDGE_SERVER_NAME")
+    response=$(curl -s -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$CLOUD_NAME/integration/rest/edge/runtimes?page=1&limit=19&searchKey=$REMOTE_EDGE_SERVER_NAME")
     echo "RESPONSE: $response"
     if [ -z "$response" ]; then
         echo "The response is empty or null"
@@ -30,7 +30,7 @@ delete_existing_edge_runtime_from_tenant(){
             echo "Deleting the perftest edge server from the tenant with agent ID: $agentID"
             response=""
             echo "curl -s -X DELETE -H "authtoken:$AUTH_TOKEN""
-            response=$(curl -s -X DELETE -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$TENANT_URL/integration/rest/edge/runtimes/$agentID/deregister?force=true")
+            response=$(curl -s -X DELETE -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$CLOUD_NAME/integration/rest/edge/runtimes/$agentID/deregister?force=true")
             echo "RESPONE: $response"
         fi
     fi
@@ -46,7 +46,7 @@ verify_edge_runtime_on_tenant_runtime_dashboard(){
     duration=$((3 * 60))
     
     # request to run
-    response=$(curl -s -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$TENANT_URL/integration/rest/edge/runtimes/$agentID/instances/")
+    response=$(curl -s -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$CLOUD_NAME/integration/rest/edge/runtimes/$agentID/instances/")
     echo "RESPONSE: $response"
     status=$(echo "$response" | grep -oP '(?<="status":")[^"]*')
     
@@ -57,7 +57,7 @@ verify_edge_runtime_on_tenant_runtime_dashboard(){
             break
         else
             echo "Edge server status: $status, waiting for the status to change, retrying..."
-            response=$(curl -s -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$TENANT_URL/integration/rest/edge/runtimes/$agentID/instances/")
+            response=$(curl -s -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$CLOUD_NAME/integration/rest/edge/runtimes/$agentID/instances/")
             echo "RESPONSE: $response"
             status=$(echo "$response" | grep -oP '(?<="status":")[^"]*')
         fi
@@ -76,7 +76,7 @@ verify_edge_runtime_on_tenant_runtime_dashboard(){
 configure_edge_runtime_to_tenant(){
      echo "configure the edge server"
      echo "curl -s -H "authtoken:$AUTH_TOKEN""
-     response=$(curl -s -X POST -H "Content-Type: application/json" -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" -d "{\"name\":\"$REMOTE_EDGE_SERVER_NAME\",\"description\":\"Remote edge runtime for performance test\"}" "https://$TENANT_URL/integration/rest/edge/runtimes?pairInstance=true")
+     response=$(curl -s -X POST -H "Content-Type: application/json" -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" -d "{\"name\":\"$REMOTE_EDGE_SERVER_NAME\",\"description\":\"Remote edge runtime for performance test\"}" "https://$CLOUD_NAME/integration/rest/edge/runtimes?pairInstance=true")
 
 #curl -s -X POST -H "Content-Type: application/json" -H "authtoken:fl92c35f89af67a433f71657" -H "cookie:route=1711024777.792.39.1540|58fb03e5a1678771a75dea209604055b;JSESSIONID=9BB0BAB6615AFA902A3FC61A2C5EBD75;login=;lang=en;UserType=Platform" -H "X-csrf-token:dbc5be18-b7b9-4f20-aa02-ed8ca1fcd075" -d "{\"name\":\"perftest123\",\"description\":\"Remote edge runtime for performance test\"}" "https://originawsint2.int-aw-us1.webmethods-int.io/integration/rest/edge/runtimes?pairInstance=true"
     if [ -z "$response" ]; then
@@ -90,7 +90,7 @@ configure_edge_runtime_to_tenant(){
         else
             echo "The pairing code: $pairingCode"
             echo "creating edge server with pairing code and setting the edge server heap min & max to 4GB"
-            docker run -p 5555:5555 -d -e SAG_IS_CLOUD_REGISTER_URL=https://$TENANT_URL -e JAVA_MIN_MEM=4096M -e JAVA_MAX_MEM=4096M -e SAG_IS_EDGE_CLOUD_ALIAS=EdgeRuntime_$REMOTE_EDGE_SERVER_NAME -e SAG_IS_CLOUD_REGISTER_TOKEN=$pairingCode --name=$REMOTE_EDGE_SERVER_NAME iregistry.eur.ad.sag/aim/msr-edge-runtime:int
+            docker run -p 5555:5555 -d -e SAG_IS_CLOUD_REGISTER_URL=https://$CLOUD_NAME -e JAVA_MIN_MEM=4096M -e JAVA_MAX_MEM=4096M -e SAG_IS_EDGE_CLOUD_ALIAS=EdgeRuntime_$REMOTE_EDGE_SERVER_NAME -e SAG_IS_CLOUD_REGISTER_TOKEN=$pairingCode --name=$REMOTE_EDGE_SERVER_NAME iregistry.eur.ad.sag/aim/msr-edge-runtime:int
             echo "started docker container"
 #           docker run -p 5555:5555 -d -e SAG_IS_CLOUD_REGISTER_URL=https://originawsint2.int-aw-us1.webmethods-int.io -e SAG_IS_EDGE_CLOUD_ALIAS=EdgeRuntime_test2123 -e SAG_IS_CLOUD_REGISTER_TOKEN=9ea88587469842928bec4390eff6aa7f8979cfb0589c4ed2af1e66d21badfc65 --name=test2123 iregistry.eur.ad.sag/aim/msr-edge-runtime:int
             verify_edge_runtime_on_tenant_runtime_dashboard
@@ -101,7 +101,7 @@ configure_edge_runtime_to_tenant(){
 fetch_tokens_from_tenant(){
     echo "Fetch auth, cookie, X-csrf-token"
     rm -f token.json
-    curl -s -u ${TENANT_USERNAME}:${TENANT_PASSWORD} https://$TENANT_URL/enterprise/v1/user/token > token.json
+    curl -s -u ${TENANT_USERNAME}:${TENANT_PASSWORD} https://$CLOUD_NAME/enterprise/v1/user/token > token.json
     AUTH_TOKEN=`cat token.json | grep -oP '(?<="authtoken":")[^"]*'`
     COOKIE=`cat token.json | grep -oP '(?<="cookie":")[^"]*'`
     CSRF=`cat token.json | grep -oP '(?<="csrf":")[^"]*'`
@@ -109,24 +109,24 @@ fetch_tokens_from_tenant(){
 
 TENANT_USERNAME=$1
 TENANT_PASSWORD=$2
-TENANT_URL=$3
-REMOTE_EDGE_SERVER_NAME=$4
+source ./origin.properties # CLOUD_NAME, REMOTE_EDGE_SERVER_NAME will be picked up as environment variables from ./origin.properties file
+echo "CLOUD_NAME: $CLOUD_NAME"
+echo "REMOTE_EDGE_SERVER_NAME: $REMOTE_EDGE_SERVER_NAME"
 echo " Check the docker version "
 docker --version
 CURRENT_DIRECTORY=$PWD
 
 if [[ $1 == *"pull_edge_runtime_image"* ]]; then
-        pull_edge_runtime_image $CURRENT_DIRECTORY
+        pull_edge_runtime_image 
 fi
 if [[ $1 == *"load_edge_runtime_image"* ]]; then
-        load_edge_runtime_image $CURRENT_DIRECTORY
+        load_edge_runtime_image 
 fi
-
 if [[ $1 == *"delete_existing_edge_runtime_from_tenant"* ]]; then
         fetch_tokens_from_tenant
-        delete_existing_edge_runtime_from_tenant $CURRENT_DIRECTORY
+        delete_existing_edge_runtime_from_tenant
 fi
 if [[ $1 == *"configure_edge_runtime_on_tenant"* ]]; then
         fetch_tokens_from_tenant
-        configure_edge_runtime_on_tenant $CURRENT_DIRECTORY
+        configure_edge_runtime_on_tenant 
 fi
--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 prerequisite(){
-        file="./origin.properties"
-        while IFS='=' read -r key value; do
-                echo "key: $key, value: $value"
-                sed -i "s*$key*$value*g" JMeterScripts/CreateEdgeServer.jmx
-        done < "$file"
-        $JMETER_HOME/bin/jmeter.sh -n -t JMeterScripts/CreateEdgeServer.jmx -Jthreads=1
+       # file="./origin.properties"
+       # while IFS='=' read -r key value; do
+       #         echo "key: $key, value: $value"
+       #         sed -i "s*$key*$value*g" JMeterScripts/CreateEdgeServer.jmx
+       # done < "$file"
+       # $JMETER_HOME/bin/jmeter.sh -n -t JMeterScripts/CreateEdgeServer.jmx -Jthreads=1
 }
 prepare_jmeter_script()
 {
@@ -15,18 +15,69 @@ prepare_jmeter_script()
                 echo "key: $key, value: $value"
                 sed -i "s*$key*$value*g" JMeterScripts/"$workload".jmx
         done < "$file"
-        sed -i "s*PATH*$endpointpath*g" JMeterScripts/"$workload".jmx
+        #sed -i "s*PATH*$endpointpath*g" JMeterScripts/"$workload".jmx
+        echo "Fetch auth, cookie, X-csrf-token"
+        rm -f token.json
+        curl -s -u ${TENANT_USERNAME}:${TENANT_PASSWORD} https://$CLOUD_NAME/enterprise/v1/user/token > token.json
+        
+        AUTH_TOKEN=`cat token.json | grep -oP '(?<="authtoken":")[^"]*'`
+        COOKIE=`cat token.json | grep -oP '(?<="cookie":")[^"]*'`
+        CSRF=`cat token.json | grep -oP '(?<="csrf":")[^"]*'`
+        #https://originawsint2.int-aw-us1.webmethods-int.io/enterprise/v1/projects?limit=19&skip=0&q=PerformanceTest
+        PROJECT_ID=$(curl -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$CLOUD_NAME/enterprise/v1/projects?limit=19&skip=0&q=$PROJECT_NAME" | grep -oP '(?<="ic_project_name":")[^"]*')
+        if [ -z "$PROJECT_ID" ]; then
+            echo "PROJECT_ID is empty or null"
+        else
+            echo "PROJECT_ID: $PROJECT_ID"
+        fi
+        AGENT_ID==$(curl -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" "https://$CLOUD_NAME/integration/rest/edge/runtimes?page=1&limit=19&searchKey=$REMOTE_EDGE_SERVER_NAME" | grep -oP '(?<="agentID":")[^"]*')
+        if [ -z "$AGENT_ID" ]; then
+            echo "AGENT_ID is empty or null"
+        else
+            echo "AGENT_ID: $AGENT_ID"
+        fi
+        
+        echo "REQUEST 1"
+        RESPONSE_OUTPUT=$(curl -s -X POST -H "Content-Type: application/json" -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" -d "{\"agentID\":\"default\",\"agentGroup\":\"Default\",\"apiEndPoint\":\"/scaffolding/agentManifest\",\"httpMethod\":\"POST\",\"input\":{\"agentId\":\"$AGENT_ID\",\"tags\":\"VaryingPayload\",\"services\":[{\"serviceName\":\"project.performancetests.integrations:VaryingPayload\"}]}}" "https://$CLOUD_NAME/integration/rest/edge/flow/admin-proxy")
+        echo "REQUEST 1, RESPONSE: $RESPONSE_OUTPUT"
+        
+        echo "REQUEST 2"
+        RESPONSE_OUTPUT=$(curl -s -X POST -H "Content-Type: application/json" -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" -d "{\"apiEndPoint\":\"/scaffolding/sync\",\"agentID\":\"$AGENT_ID\",\"agentGroup\":\"default\",\"httpMethod\":\"POST\",\"input\":{\"agentId\":\"$AGENT_ID\",\"enableConnections\":true}}" "https://$CLOUD_NAME/integration/rest/edge/flow/admin-proxy")
+        echo "REQUEST 2, RESPONSE: $RESPONSE_OUTPUT"
+        
+        echo "REQUEST 3"
+        RESPONSE_OUTPUT=$(curl -s -X POST -H "Content-Type: application/json" -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" -d "{\"apiEndPoint\":\"/package/PerformanceTestsProject\",\"agentID\":\"default\",\"agentGroup\":\"default\",\"httpMethod\":\"GET\"}" "https://$CLOUD_NAME/integration/rest/edge/flow/admin-proxy")
+        echo "REQUEST 3, RESPONSE: $RESPONSE_OUTPUT"
+        
+        echo "REQUEST 4"
+        RESPONSE_OUTPUT=$(curl -s -X POST -H "Content-Type: application/json" -H "authtoken:$AUTH_TOKEN" -H "cookie:$COOKIE" -H "X-csrf-token:$CSRF" -d "{\"apiEndPoint\":\"/package/PerformanceTestsProject\",\"agentID\":\"$AGENT_ID\",\"agentGroup\":\"default\",\"httpMethod\":\"GET\"}" "https://$CLOUD_NAME/integration/rest/edge/flow/admin-proxy")
+        echo "REQUEST 4, RESPONSE: $RESPONSE_OUTPUT"
+        
+        sed -i "s*AGENT_ID*$AGENT_ID*g" JMeterScripts/"$workload".jmx
+        sed -i "s*PROJECT_ID*$PROJECT_ID*g" JMeterScripts/"$workload".jmx
+        sed -i "s*AUTH_TOKEN*$AUTH_TOKEN*g" JMeterScripts/"$workload".jmx
+        sed -i "s*COOKIE*$COOKIE*g" JMeterScripts/"$workload".jmx
+        sed -i "s*CSRF*$CSRF*g" JMeterScripts/"$workload".jmx
 }
 
 execute_jmeter_script()
 {
-        echo " execute jmeter script "
-        $JMETER_HOME/bin/jmeter.sh -n -t JMeterScripts/$1.jmx -Jthreads=$2 -Jduration=$3 -l TEMP_DIRECTORY/$1.jtl
+        echo " execute jmeter script $testcase"
+        $JMETER_HOME/bin/jmeter.sh -n -t JMeterScripts/$2.jmx -Jthreads=$3 -Jduration=$4 -l TEMP_DIRECTORY/$1.jtl
         java -jar $JMETER_HOME/lib/cmdrunner-2.2.jar --tool Reporter --generate-csv TEMP_DIRECTORY/$1.csv --input-jtl TEMP_DIRECTORY/$1.jtl --plugin-type SynthesisReport
+        cat $5/$testcase.csv
+        rm -f TEMP_DIRECTORY/$1.jtl
+        java -DWORKLOAD_NAME=$1 -DTEMP_DIRECTORY=$5/ -cp bpt_utils.jar perf.bpt.util.CompareBaseline
 }
 
-File="TestScripts/Benchmark_testcase_All.csv"
+source ./origin.properties # CLOUD_NAME, REMOTE_EDGE_SERVER_NAME will be picked up as environment variables from ./origin.properties file
+echo "CLOUD_NAME: $CLOUD_NAME"
+echo "REMOTE_EDGE_SERVER_NAME: $REMOTE_EDGE_SERVER_NAME"
+File="Tests/Benchmark_testcase_All.csv"
 JMETER_HOME=/home/centos/apache-jmeter-5.5
+TEMP_DIRECTORY=$1
+TENANT_USERNAME=$2
+TENANT_PASSWORD=$3
 Lines=$(cat $File)
 for Line in $Lines
 do
@@ -44,5 +95,5 @@ do
         echo "workload: $workload"
         cp JMeterScripts/$testcase.jmx JMeterScripts/$workload.jmx
         prepare_jmeter_script $workload
-        execute_jmeter_script $workload $concurrentusers $duration
+        execute_jmeter_script $testcase $workload $concurrentusers $duration $TEMP_DIRECTORY
 done
